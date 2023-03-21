adaaeb51feb86945b2e7876e7c60c2743c172d0b
carusina <msjjang0315@naver.com>
2023-03-22 00:42:54 +0900

    15552-빠른 A+B

---
+++ faster_A+B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int A[T], B[T];
+
+    for(int i = 0; i < T; i++)
+        cin >> A[i] >> B[i];
+    for(int i = 0; i < T; i++)
+        cout << A[i] + B[i] << "\n";
+}
