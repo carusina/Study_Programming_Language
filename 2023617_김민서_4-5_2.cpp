f61244abeb4d744f331d23d9cf3bc365506c9a7d
carusina <msjjang0315@naver.com>
2023-04-05 15:49:15 +0900

    04/05 C++ 과제-포인터 4장-05번

---
+++ 2023617_김민서_4-5_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void myswap(double &c, double &d)
+{
+    double tmp;
+    tmp = c;
+    c = d;
+    d = tmp;
+}
+
+int main()
+{
+    double a, b;
+    cout << "두 수 입력: ";
+    cin >> a >> b;
+    cout << "a: " << a << ", b: " << b << endl;
+
+    myswap(a, b);
+    cout << "a: " << a << ", b: " << b << endl;
+}
